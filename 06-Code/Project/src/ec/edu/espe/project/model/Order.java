e0a0794919543e2730fae70d4daa0d0820e10ea2
AlexDario998 <alexecuador998@gmail.com>
2020-06-20 19:57:10 -0500

    v1.4.1

--- 06-Code/Project/src/ec/edu/espe/project/model/Order.java
+++ 06-Code/Project/src/ec/edu/espe/project/model/Order.java
@@ -12,33 +12,29 @@ import java.util.Scanner;
  * @author hp
  */
 public class Order {
-    Scanner dataEntry = new Scanner(System.in);
-    
-    Combo combo= new Combo();
-    Beverage beverage = new Beverage();
-    Snack snack = new Snack();    
-    
-    double[]priceVector ={4.0,8.5,8.5,8.5,8.5,11.0,12.5,14.0,13.5,34.0,15.0,23.0,
-        15.0,15.0,13.0,15.0,16.0,25.0,7.5,14.0,26.0,68.0};
-    
-    public double getBeverage(){
-    int [] code;
-    int i;
-    int beverageLimit;
-        System.out.println("How many beverage do you want? ");
-        beverageLimit=dataEntry.nextInt();
-        if(beverageLimit==0){           
-            return 0;
-        }else{       
-            do{
-                for(i=0;i<=beverageLimit;i++){
-                code[] = beverage.choose();
-                System.out.println("Do you want to add more beverages? Yes=1/No=0");
-                
 
-                }    
-            }while(); 
-         
-        }
-    }   
+    Scanner dataEntry = new Scanner(System.in);
+
+    Combo combo = new Combo();
+    Beverage beverage = new Beverage();
+    Snack snack = new Snack();
+
+    double[] priceVector = {4.0, 8.5, 8.5, 8.5, 8.5, 11.0, 12.5, 14.0, 13.5, 34.0, 15.0, 23.0,
+        15.0, 15.0, 13.0, 15.0, 16.0, 25.0, 7.5, 14.0, 26.0, 68.0};
+
+    public double getBeverage() {
+        int[] code;
+        int i;
+        int beverageLimit;
+        System.out.println("How many beverage do you want? ");
+        beverageLimit = dataEntry.nextInt();
+
+        do {
+            for (i = 0; i <= beverageLimit; i++) {
+                code[] = beverage.choose();
+
+            }
+        } while ();
+
+    }
 }
