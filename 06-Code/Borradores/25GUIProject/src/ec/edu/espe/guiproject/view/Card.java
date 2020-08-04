31711be6505f09ac0b425803f4ccdf0d15b1eaca
AlexDario998 <alexecuador998@gmail.com>
2020-08-04 00:44:46 -0500

    interface snack, card

---
+++ 06-Code/Borradores/25GUIProject/src/ec/edu/espe/guiproject/view/Card.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.guiproject.view;
+
+/**
+ *
+ * @author Alex Velastegui ESPE-DCCO
+ */
+public class Card {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Card(String description) {
+        this.description = description;
+    }
+    
+    
+    
+}
